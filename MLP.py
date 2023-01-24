19f76229dea8ba11f9f1d4deae69dea2ef739219
SofiaG73 <ana.s.guerreiro@tecnico.ulisboa.pt>
2023-01-24 10:36:01 +0000

    run yapf on both files

--- MLP.py
+++ MLP.py
@@ -7,7 +7,9 @@ Created on Fri Jan 13 17:55:01 2023
 
 import torch.nn as nn
 
+
 class MLP(nn.Module):
+
     def __init__(self, input_size, hidden_size, output_size):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
@@ -21,4 +23,3 @@ class MLP(nn.Module):
         # x = self.tanh(x)
         x = self.fc2(x)
         return x
-    
--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import matplotlib.pyplot as plt
 
 # for PL binding
 
-
 mydir_aff = '../../datasets/index/INDEX_general_PL_data.2020'
 mydir = '../../datasets/refined-set'
 
@@ -24,9 +23,12 @@ dataset = PDBDataset(pdb_files, aff_dict)
 
 train_size = int(0.8 * len(dataset))
 test_size = len(dataset) - train_size
-train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+train_dataset, test_dataset = torch.utils.data.random_split(
+    dataset, [train_size, test_size])
 
-dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=30, shuffle=True)
+dataloader = torch.utils.data.DataLoader(train_dataset,
+                                         batch_size=30,
+                                         shuffle=True)
 
 input_dim = len(dataset[0][0])
 hidden_dim = 15
@@ -37,14 +39,13 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 num_epochs = 70
 loss_values = []
 
-
 loss_values = []
 for epoch in range(num_epochs):
     epoch_loss = 0
     for inputs, targets in dataloader:
         # Forward pass
         outputs = model(inputs)
-        loss = mse_loss(outputs, torch.unsqueeze(targets,-1))
+        loss = mse_loss(outputs, torch.unsqueeze(targets, -1))
         epoch_loss += loss.item()
 
         # Backward and optimize
@@ -52,7 +53,7 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
     loss_values.append(epoch_loss / len(dataloader))
-    
+
 print('training complete')
 
 plt.plot(loss_values)
