d8161bdb76e538754c4338e8bb4125d632507c5a
SofiaG73 <ana.s.guerreiro@tecnico.ulisboa.pt>
2023-01-27 15:05:28 +0000

    adding more flags

--- main.py
+++ main.py
@@ -24,6 +24,12 @@ flags.DEFINE_string('aff_dir',
 flags.DEFINE_string('data_dir', '../../datasets/refined-set',
                     'specify the path to the dataset')
 
+flags.DEFINE_float('train_size', 0.8, 'percentage of train-validation-split')
+
+flags.DEFINE_integer('batch_size', 30, 'batch size')
+
+flags.DEFINE_integer('hidden_size', 15, 'size of the hidden layer')
+
 FLAGS = flags.FLAGS
 FLAGS(sys.argv)
 # for PL binding
@@ -32,23 +38,22 @@ aff_dict = get_affinities(FLAGS.aff_dir)
 pdb_files = read_dataset(FLAGS.data_dir)
 dataset = PDBDataset(pdb_files, aff_dict)
 
-train_size = int(0.8 * len(dataset))
+train_size = int(FLAGS.train_size * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = torch.utils.data.random_split(
     dataset, [train_size, test_size])
 
 dataloader = torch.utils.data.DataLoader(train_dataset,
-                                         batch_size=30,
+                                         batch_size=flags.batch_size,
                                          shuffle=True)
 
 input_dim = len(dataset[0][0])
-hidden_dim = 15
+hidden_dim = flags.hidden_size
 output_dim = 1
 model = MLP(input_dim, hidden_dim, output_dim)
 mse_loss = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 num_epochs = 70
-loss_values = []
 
 loss_values = []
 for epoch in range(num_epochs):
