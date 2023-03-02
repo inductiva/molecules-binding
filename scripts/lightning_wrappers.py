29f81aa02973d20314e36095ea5f2e54e1d8cadb
SofiaG73 <ana.s.guerreiro@tecnico.ulisboa.pt>
2023-03-02 16:26:40 +0000

    refactoring lightning wrapper

--- scripts/lightning_wrappers.py
+++ scripts/lightning_wrappers.py
@@ -21,6 +21,7 @@ flags.DEFINE_multi_integer("num_hidden", [40, 30, 30, 40],
                            "size of the new features after conv layer")
 flags.DEFINE_float("train_perc", 0.8, "percentage of train-validation-split")
 flags.DEFINE_integer("batch_size", 32, "batch size")
+flags.DEFINE_integer("num_epochs", 100, "number of epochs")
 
 criterion = torch.nn.MSELoss()
 
@@ -30,12 +31,18 @@ class AffinityBindingTrainer(pl.LightningModule):
         Graph Convolution Neural Network
     """
 
-    def __init__(self, model, learning_rate, train_dataset, batch_size):
+    def __init__(self, model, learning_rate, dataset, batch_size, train_perc):
         super().__init__()
 
         self.model = model
         self.learning_rate = learning_rate
-        self.train_dataset = train_dataset
+
+        train_size = int(train_perc * len(dataset))
+        test_size = len(dataset) - train_size
+        self.train_dataset, self.val_dataset = torch.utils.data.random_split(
+            dataset, [train_size, test_size],
+            generator=torch.Generator().manual_seed(42))
+
         self.batch_size = batch_size
 
     def training_step(self, data, _):
@@ -50,23 +57,40 @@ class AffinityBindingTrainer(pl.LightningModule):
     def train_dataloader(self):
         train_loader = DataLoader(self.train_dataset,
                                   batch_size=self.batch_size,
+                                  num_workers=12,
                                   shuffle=True)
         return train_loader
+
+    def validation_step(self, data, _):
+        labels = data.y.unsqueeze(1)
+        outputs = self.model(data, data.batch, FLAGS.dropout_rate)
+        loss = criterion(labels, outputs)
+        return {"val_loss": loss}
+
+    def val_dataloader(self):
+        val_loader = DataLoader(self.val_dataset,
+                                batch_size=self.batch_size,
+                                num_workers=12,
+                                shuffle=False)
+        return val_loader
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        return {"val_loss": avg_loss}
 
 
 def main(_):
     dataset = torch.load(FLAGS.path_dataset)
-    train_size = int(FLAGS.train_perc * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, _ = torch.utils.data.random_split(
-        dataset, [train_size, test_size],
-        generator=torch.Generator().manual_seed(42))
 
-    trainer = Trainer(fast_dev_run=False)
+    trainer = Trainer(fast_dev_run=False,
+                      max_epochs=FLAGS.num_epochs,
+                      accelerator="gpu",
+                      devices=1)
     model = GraphNN(FLAGS.num_hidden, num_features)
     model.double()
     lightning_model = AffinityBindingTrainer(model, FLAGS.learning_rate,
-                                             train_dataset, FLAGS.batch_size)
+                                             dataset, FLAGS.batch_size,
+                                             FLAGS.train_perc)
     trainer.fit(lightning_model)
 
 
