771a80ccfb730efae52d9d401bce38d009f9de3e
SofiaG73 <ana.s.guerreiro@tecnico.ulisboa.pt>
2023-03-10 17:22:15 +0000

    starting to add mlflow

---
+++ molecules_binding/callbacks.py
@@ -0,0 +1,25 @@
+""" Callbacks for training"""
+from pytorch_lightning.callbacks import Callback
+import mlflow
+
+
+class LossMonitor(Callback):
+    """Logs loss and model checkpoints to mlflow."""
+
+    def __init__(self, save_dir, run_id, log_frequency=1):
+        self.save_dir = save_dir
+        self.run_id = run_id
+        self.batch_loss = []
+        self.log_frequency = log_frequency
+
+    def on_train_epoch_end(self, trainer, _):
+        # Getting the metrics.
+        validation_loss = trainer.callback_metrics['val_loss']
+        training_loss = trainer.callback_metrics['loss']
+        metrics = {
+            'metrics_validation_loss': validation_loss.item(),
+            'metrics_training_loss': training_loss.item()
+        }
+
+        with mlflow.start_run(run_id=self.run_id):
+            mlflow.log_metrics(metrics, step=trainer.current_epoch)
--- scripts/train_graphnet_lightning.py
+++ scripts/train_graphnet_lightning.py
@@ -3,6 +3,7 @@ Lightning Code
 """
 import torch
 from molecules_binding.models import GraphNN
+from molecules_binding.callbacks import LossMonitor
 # from molecules_binding.parsers import num_features
 from molecules_binding.lightning_wrapper import GraphNNLightning
 from torch_geometric.loader import DataLoader
@@ -18,17 +19,19 @@ flags.DEFINE_string("path_dataset", None,
 flags.mark_flag_as_required("path_dataset")
 flags.DEFINE_float("learning_rate", 0.001, "learning rate")
 flags.DEFINE_float("dropout_rate", 0.3, "Dropout rate")
-flags.DEFINE_multi_integer("num_hidden", [40, 30, 30, 40],
-                           "size of the new features after conv layer")
+flags.DEFINE_list("num_hidden", [40, 30, 30, 40],
+                  "size of the new features after conv layer")
 flags.DEFINE_float("train_perc", 0.8, "percentage of train-validation-split")
 flags.DEFINE_integer("batch_size", 32, "batch size")
 flags.DEFINE_integer("num_epochs", 100, "number of epochs")
 flags.DEFINE_integer("num_workers", 12, "number of workers")
 flags.DEFINE_bool("use_gpu", True, "True if using gpu, False if not")
 
+
 def _log_parameters(**kwargs):
     for key, value in kwargs.items():
         mlflow.log_param(str(key), value)
+
 
 def main(_):
     dataset = torch.load(FLAGS.path_dataset)
@@ -47,24 +50,28 @@ def main(_):
                             num_workers=FLAGS.num_workers,
                             shuffle=False)
 
-    model = GraphNN(FLAGS.num_hidden, dataset[0].num_node_features)
+    layer_sizes = list(map(int, FLAGS.num_hidden))
+    model = GraphNN(layer_sizes, dataset[0].num_node_features)
     model.double()
 
     lightning_model = GraphNNLightning(model, FLAGS.learning_rate,
                                        FLAGS.batch_size, FLAGS.dropout_rate)
-    
+
     with mlflow.start_run():
-        _log_parameters(
-            batch_size = FLAGS.batch_size,
-            learning_rate = FLAGS.learning_rate,
-            dropout_rate = FLAGS.dropout_rate,
-            num_hidden = FLAGS.num_hidden
-            )
-    
+        _log_parameters(batch_size=FLAGS.batch_size,
+                        learning_rate=FLAGS.learning_rate,
+                        dropout_rate=FLAGS.dropout_rate,
+                        num_hidden=FLAGS.num_hidden)
+        run_id = mlflow.active_run().info.run_id
+
+        loss_callback = LossMonitor(run_id, 50)
+        callbacks = [loss_callback]
+
     trainer = Trainer(fast_dev_run=False,
                       max_epochs=FLAGS.num_epochs,
                       accelerator="gpu" if FLAGS.use_gpu else None,
-                      devices=1)
+                      devices=1,
+                      callbacks=callbacks)
     trainer.fit(model=lightning_model,
                 train_dataloaders=train_loader,
                 val_dataloaders=val_loader)
