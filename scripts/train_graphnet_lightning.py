161b5b40c2eafe80d266ef53cf2979280da46aec
SofiaG73 <ana.s.guerreiro@tecnico.ulisboa.pt>
2023-06-06 12:11:26 +0100

    adding shuffle nodes possibility to graphnet

--- scripts/train_graphnet_lightning.py
+++ scripts/train_graphnet_lightning.py
@@ -77,37 +77,38 @@ def main(_):
         for i in range(len(dataset)):
             dataset[i].edge_attr = None
 
-    if FLAGS.sanity_check_rotation:
-        for i in range(len(val_dataset)):
-            center_rotation = val_dataset[i].pos.mean(axis=0)
+    # if FLAGS.sanity_check_rotation:
+    #     for i in range(len(val_dataset)):
+    #         center_rotation = val_dataset[i].pos.mean(axis=0)
 
-            angle = torch.tensor(45)  # Rotation angle in degrees
-            axis = torch.tensor([0, 0, 1])  # Rotation axis (e.g., Z-axis)
-            angle_rad = torch.deg2rad(angle)
-            cos_theta = torch.cos(angle_rad)
-            sin_theta = torch.sin(angle_rad)
-            u_x, u_y, u_z = axis
+    #         angle = torch.tensor(45)  # Rotation angle in degrees
+    #         axis = torch.tensor([0, 0, 1])  # Rotation axis (e.g., Z-axis)
+    #         angle_rad = torch.deg2rad(angle)
+    #         cos_theta = torch.cos(angle_rad)
+    #         sin_theta = torch.sin(angle_rad)
+    #         u_x, u_y, u_z = axis
 
-            rotation_matrix = torch.tensor(
-                [[
-                    cos_theta + (u_x**2) * (1 - cos_theta),
-                    u_x * u_y * (1 - cos_theta) - u_z * sin_theta,
-                    u_x * u_z * (1 - cos_theta) + u_y * sin_theta
-                ],
-                 [
-                     u_y * u_x * (1 - cos_theta) + u_z * sin_theta,
-                     cos_theta + (u_y**2) * (1 - cos_theta),
-                     u_y * u_z * (1 - cos_theta) - u_x * sin_theta
-                 ],
-                 [
-                     u_z * u_x * (1 - cos_theta) - u_y * sin_theta,
-                     u_z * u_y * (1 - cos_theta) + u_x * sin_theta,
-                     cos_theta + (u_z**2) * (1 - cos_theta)
-                 ]])
-            # rotated coords
-            val_dataset[i].pos = torch.matmul(
-                val_dataset[i].pos - center_rotation,
-                rotation_matrix) + center_rotation
+    #         rotation_matrix = torch.tensor(
+    #             [[
+    #                 cos_theta + (u_x**2) * (1 - cos_theta),
+    #                 u_x * u_y * (1 - cos_theta) - u_z * sin_theta,
+    #                 u_x * u_z * (1 - cos_theta) + u_y * sin_theta
+    #             ],
+    #              [
+    #                  u_y * u_x * (1 - cos_theta) + u_z * sin_theta,
+    #                  cos_theta + (u_y**2) * (1 - cos_theta),
+    #                  u_y * u_z * (1 - cos_theta) - u_x * sin_theta
+    #              ],
+    #              [
+    #                  u_z * u_x * (1 - cos_theta) - u_y * sin_theta,
+    #                  u_z * u_y * (1 - cos_theta) + u_x * sin_theta,
+    #                  cos_theta + (u_z**2) * (1 - cos_theta)
+    #              ]])
+    #         # rotated coords
+    #         val_dataset[i].pos = torch.matmul(
+    #             val_dataset[i].pos - center_rotation,
+    #             rotation_matrix) + center_rotation
+
 
     train_loader = DataLoader(train_dataset,
                               batch_size=FLAGS.batch_size,
