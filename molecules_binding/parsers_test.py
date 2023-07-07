832cc500e1a7422c3e7b0471c4a0f2c4d7ee9a63
SofiaG73 <ana.s.guerreiro@tecnico.ulisboa.pt>
2023-07-07 11:18:07 +0100

    tried to test structural info, but couldnt because pytest and numba have conflicts

--- molecules_binding/parsers_test.py
+++ molecules_binding/parsers_test.py
@@ -1,8 +1,11 @@
 """ test parsers functions on an element of the dataset"""
 from molecules_binding.parsers import get_affinities
+from molecules_binding.datasets_interaction import structural_info
 import pytest
+import numpy as np
 
 path_example_index = "example_dataset/index_data_example.2020"
+a, b, c = np.array([0, 0, 0]), np.array([1, 0, 0]), np.array([0, 1, 0])
 
 
 @pytest.mark.parametrize("affinity_dict, expected_dict",
@@ -18,4 +21,7 @@ def test_get_affinities(affinity_dict, expected_dict):
     assert affinity_dict == expected_dict
 
 
-# TODO (Sofia): test graphs creation
+@pytest.mark.benchmark(group="structural_info")
+def test_structural_info(benchmark, espected_structural_info=(90.0, 0.5, 1.0)):
+    result = benchmark(structural_info, a, b, c)
+    assert result == espected_structural_info
