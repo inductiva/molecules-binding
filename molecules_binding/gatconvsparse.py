f393dda6bde3ef2d2a55fc8e4911549c4f8698ca
SofiaG73 <ana.s.guerreiro@tecnico.ulisboa.pt>
2023-08-02 10:21:27 +0100

    sparsemax implementation in GAT layer

--- molecules_binding/gatconvsparse.py
+++ molecules_binding/gatconvsparse.py
@@ -1,15 +1,55 @@
-from torch_geometric.nn import GATConv
-from sparsemax import Sparsemax
+"""Redefining GATConv with sparsemax activation instead of softmax"""
 from typing import Optional
-
-import torch.nn.functional as F
+import torch
 from torch import Tensor
-
+import torch.nn.functional as F
+# from torch_geometric.utils import softmax
 from torch_geometric.typing import OptTensor
+from torch_geometric.nn import GATConv
+
+
+
+def sparsemax(a: Tensor) -> Tensor:
+    zs = torch.sort(a, descending=True, dim=0).values
+    size = zs.size(0)
+    indices = torch.arange(start=1,
+                         end=size + 1,
+                         step=1,
+                         dtype=int,
+                         device=a.device).reshape(size, 1)
+    bound = torch.as_tensor(1, device=a.device) + indices * zs
+    cum_sum_zs = torch.cumsum(zs, dim=0)
+    is_ge = torch.ge(bound, cum_sum_zs)
+    k = torch.max(is_ge * indices)
+    tau = (cum_sum_zs[k - 1] - torch.as_tensor(1, device=a.device)) / k
+    return torch.relu(a - tau)
+
+
+def sparsemax_pyg(src, index, ptr, size_i) -> Tensor:
+    unique_indices = torch.unique(index)
+    # print("index foi usado")
+    result = torch.zeros_like(src)
+
+    for i in unique_indices:
+        mask = index == i
+        result[mask] = sparsemax(src[mask])
+    return result
+
+
+# def sparsemax_pyg(src, index, ptr, size_i) -> Tensor:
+#     count_per_index = torch.bincount(index)
+
+#     indexes_sorted = torch.sort(index).indices
+
+#     separated_tensors = torch.split(src[indexes_sorted],
+#                                     count_per_index.tolist())
+#     separated_outputs = list(map(sparsemax, separated_tensors))
+#     output = torch.cat(separated_outputs, dim=0)[indexes_sorted.argsort()]
+#     return output
 
 
 class GATConvSparsemax(GATConv):
-
+    """ GAT layer with sparsemax activation instead of softmax"""
     def edge_update(self, alpha_j: Tensor, alpha_i: OptTensor,
                     edge_attr: OptTensor, index: Tensor, ptr: OptTensor,
                     size_i: Optional[int]) -> Tensor:
@@ -27,7 +67,9 @@ class GATConvSparsemax(GATConv):
             alpha = alpha + alpha_edge
 
         alpha = F.leaky_relu(alpha, self.negative_slope)
+
         # alpha = softmax(alpha, index, ptr, size_i)
-        # alpha = Sparsemax(alpha, index, ptr, size_i)
+        alpha = sparsemax_pyg(alpha, index, ptr, size_i)
+
         alpha = F.dropout(alpha, p=self.dropout, training=self.training)
         return alpha
